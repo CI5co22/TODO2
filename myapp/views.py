20b1ce2bd0a7d4810fdf1b3ba100edb4b36f7786
CI5co22 <arnoldajcip225@gmail.com>
2025-09-05 21:50:15 -0600

    Archivos modificados segun PEP8
    usando pycodestyle

--- myapp/views.py
+++ myapp/views.py
@@ -6,38 +6,30 @@ from django.http import JsonResponse
 
 def index(request):
     tareas = Tareas.objects.all()
-    
-    
+
     if request.method == "GET":
         mostrar = request.GET.get('mostrar')
         if mostrar is None:
             mostrar = 'verTodo'
-        
+
         if mostrar == 'verTodo':
             tareas = Tareas.objects.all()
         else:
             mostrar = int(mostrar)
 
-
         if mostrar == 0:
             tareas = Tareas.objects.filter(estado=0)
         elif mostrar == 1:
             tareas = Tareas.objects.filter(estado=1)
-            
-        
+
         prioridad = request.GET.get('verPrioridad')
-        
+
         if prioridad == 'Alta':
             tareas = Tareas.objects.filter(prioridad='Alta')
         elif prioridad == 'Media':
             tareas = Tareas.objects.filter(prioridad='Media')
         elif prioridad == 'Baja':
             tareas = Tareas.objects.filter(prioridad='Baja')
-            
-            
-       
-        
-
 
     return render(request, "home.html", {'lista': tareas})
 
@@ -50,33 +42,32 @@ def add_task(request):
 
         if title and status and prioridad:
             tarea = Tareas(
-            name=title,
-            estado=int(status), 
-            prioridad=prioridad
-        )
-        tarea.save()
-    
+                name=title,
+                estado=int(status),
+                prioridad=prioridad
+            )
+    tarea.save()
+
     return redirect("/")
 
 
 def update_task(request):
     idupd = request.POST.get('update')
     tarea = get_object_or_404(Tareas, id=idupd)
-    
+
     if request.method == "POST":
         tarea.name = request.POST.get('title', tarea.name)
         tarea.prioridad = request.POST.get('prioridad', tarea.prioridad)
         tarea.save()
-        
+
         return redirect("/")
-    
 
 
 def delete_task(request):
     id = request.POST.get('delete')
     tarea = get_object_or_404(Tareas, id=id)
     tarea.delete()
-    
+
     return redirect("/")
 
 
@@ -84,9 +75,8 @@ def cambiar_estado(request):
     if request.method == "POST":
         tarea_id = request.POST.get('check')
         last_estado = int(request.POST.get('lastEstado'))
-        
-        tarea = get_object_or_404(Tareas, id=tarea_id)
 
+        tarea = get_object_or_404(Tareas, id=tarea_id)
 
         if last_estado == 0:
             tarea.estado = 1
@@ -94,7 +84,7 @@ def cambiar_estado(request):
         else:
             tarea.estado = 0
             tarea.save()
-    
+
     return JsonResponse({
         'status': 'ok',
         'id': tarea.id,
